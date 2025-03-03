69fdd6f123d90a3ea044eaf6a0fe56db474c2584
Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
2025-03-03 10:37:14 +0100

    Squashed commit of the following:
    commit 02691856605673df240f8febc265635ae60f815b
    Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
    Date:   Mon Mar 3 10:36:32 2025 +0100
    
        [+] Added player trigger col
    
    commit fada88cf79145ee0200e2fede4a761bb5841318b
    Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
    Date:   Mon Mar 3 10:21:00 2025 +0100
    
        Added Leg Rotation
    
    commit 6794751e130e23d45db5b4951f10c111c8195fbc
    Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
    Date:   Mon Mar 3 09:53:19 2025 +0100
    
        [+] Input Manager Package
    
        [+] Start aan werkende movement
    
    commit 7698fd35bb64d01cf8777297d5ee44fa36cdbde0
    Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
    Date:   Thu Feb 27 11:52:26 2025 +0100
    
        [+] Player start
    
        [+] Added player prefab
        [+] Added player script
    
    commit 0b993e11450ed641a4e06de47afbbf0e4bc0022b
    Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
    Date:   Thu Feb 27 11:27:07 2025 +0100
    
        [+] Added scenes
    
        [+] Added game and achievement scene
        [+] Also added them to the available scene list thingy

---
+++ simmac/Assets/Scenes/GameScene/Scripts/Player.cs
@@ -0,0 +1,76 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    private Vector2 _velocity;
+    private Collider2D _torsoCol;
+    private GameObject _torso;
+    private Collider2D _legsCol;
+    private GameObject _legs;
+    [SerializeField] private float _speed;
+    private InputAction _move;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _torso = GameObject.Find("Torso");
+        _legs = GameObject.Find("Legs");
+        _torsoCol = _torso.GetComponent<CapsuleCollider2D>();
+        _legsCol = _legs.GetComponent<CapsuleCollider2D>();
+        _move = InputSystem.actions.FindAction("Move");
+    }
+
+    void Update()
+    {
+        HandleInput();
+        HandleLegRotation();
+        HandleTorsoRotation();
+        SlowPlayer();
+        _rb.linearVelocity = _velocity;
+    }
+
+    void HandleInput()
+    {
+        Vector2 MoveValue = _move.ReadValue<Vector2>();
+        MoveValue *= _speed;
+        MoveValue = Vector2.ClampMagnitude(MoveValue, _speed);
+        _velocity = MoveValue;
+    }
+
+    void HandleLegRotation()
+    {
+        if (_velocity != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(_velocity.x, _velocity.y) * Mathf.Rad2Deg;
+            _legs.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
+        }
+    }
+
+    void HandleTorsoRotation()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - _torso.transform.position;
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        _torso.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
+    }
+
+    void SlowPlayer()
+    {
+        OnTriggerEnter2D(_torsoCol);
+        OnTriggerEnter2D(_legsCol);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col == _torsoCol || col == _legsCol)
+        {
+            return;
+        }
+        Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+    }
+}
+
+
