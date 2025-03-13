f3991f784875fd5b2a06246dab902923c0603417
Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
2025-03-13 13:21:18 +0100

    Squashed commit of the following:
    commit aa811b634ce7e4ea118c0ac7583526f5ffb1644a
    Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
    Date:   Thu Mar 13 13:19:45 2025 +0100
    
        [-] Removed unused namespaces
    
    commit 24520a43b0956b073f69f54485a1c28ba7d4ca59
    Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
    Date:   Thu Mar 13 13:18:35 2025 +0100
    
        [+] Made working save files and functions
    
        [+] Can now save the game with a static function
    
    commit 3e1f35c833ad8da16da1dcc66e5dba6d1a2ebc45
    Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
    Date:   Thu Mar 13 12:43:47 2025 +0100
    
        [+] Working save file
    
    commit a417a178b243a8adf7b08189adfb5309d8790121
    Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
    Date:   Thu Mar 13 12:25:22 2025 +0100
    
        [+] Messagepack usage
    
        [+] Made a simple start to use message pack
    
    commit ac4adf95f2bddbd1f158c60eaba8c98473e85e3e
    Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
    Date:   Wed Mar 12 12:01:49 2025 +0100
    
        Squashed commit of the following:
    
        commit 8eb253ff2c8375de938e112bef5550d3d7c629c1
        Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
        Date:   Wed Mar 12 12:01:12 2025 +0100
    
            Squashed commit of the following:
    
            commit 93d9a84e6b6d9f0869064f2a6dcdd78c2b3a67fe
            Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
            Date:   Wed Mar 12 12:00:52 2025 +0100
    
                [+] Made a start on positioning
    
            commit 9419199c0d94638cab18ad4e7566fb2f79bbfbe4
            Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
            Date:   Wed Mar 12 11:05:27 2025 +0100
    
                [+] Can read out and add achievements
    
                [+] Added availableAchievements list that stores Achievement objects of all achievements from the json
    
            commit de59301626eb0e658e1e44e0156f3eb4891ec68e
            Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
            Date:   Wed Mar 12 10:14:57 2025 +0100
    
                [+] Added json packages and Achievement class
    
                [+] Installed package dll's with their dependecies
                [+] Added achievement UI start
                [+] Added achievement class for reading jsons
                [+] Added AchievementBehaviour class for using achievments
    
        commit 038bcdb6077e72b895a7baf47ea6744b81a2c99b
        Author: Douwe <douwewesterdijk@gmail.com>
        Date:   Wed Mar 12 10:54:34 2025 +0100
    
            [~] Update GDD
    
            [+] Minigames uitgeschreven
            [+] Toegevoegd aan inhoudsopgave
    
        commit d1376563df15768957f5ea08abef4f8aba2065e7
        Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
        Date:   Tue Mar 11 16:31:44 2025 +0100
    
            [~] Taken
    
            [+] Nolan gaat ui ook doen
    
        commit a2e0e3d790846f5739644eb6775f73a748bac5c0
        Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
        Date:   Tue Mar 11 12:12:54 2025 +0100
    
            [~] Klassendiagram over gezet naar yEd
    
            [+] ook een png snapchat van vandaag gemaakt

--- simmac/Assets/Scenes/Shared Assets/GameManager.cs
+++ simmac/Assets/Scenes/Shared Assets/GameManager.cs
@@ -1,11 +1,46 @@
+using System.IO;
+using MessagePack;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
+    private static string filePath;
+
+    [MessagePackObject]
+    public struct _GameState
+    {
+        [Key(0)]
+        public bool is_current_game;
+        [Key(1)]
+        public bool game_over;
+        [Key(2)]
+        public int current_day;
+        [Key(3)]
+        public float money;
+        [Key(4)]
+        public int customers_served;
+    }
+    _GameState current_state;
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
     void Start()
     {
-
+        filePath = Path.Combine(Application.persistentDataPath, "savegame.simmac");
+        _instance.current_state = loadCurrentGame();
+        saveCurrentGame();
     }
 
     public static GameManager Instance
@@ -21,5 +56,23 @@ public class GameManager : MonoBehaviour
     void Update()
     {
 
+    }
+
+    private _GameState loadCurrentGame()
+    {
+        try
+        {
+            return MessagePackSerializer.Deserialize<_GameState>(File.ReadAllBytes(filePath));
+        }
+        catch
+        {
+            return new _GameState { is_current_game = false, game_over = false, current_day = 0, money = 0.0f, customers_served = 0 };
+        }
+    }
+
+    public static void saveCurrentGame()
+    {
+        byte[] bytes = MessagePackSerializer.Serialize(_instance.current_state);
+        File.WriteAllBytes(filePath, bytes);
     }
 }
