77bb76a301e39dda9e77098aadf7dc1cd7c6f557
Douwe <douwewesterdijk@gmail.com>
2025-03-21 09:30:05 +0100

    Squashed commit of the following:
    commit a5513b4309fd2a5a14ec307f40fc16a50765220c
    Author: Douwe <douwewesterdijk@gmail.com>
    Date:   Fri Mar 21 09:27:11 2025 +0100
    
        [~] All Station classes done
    
    commit 1b756dfbd841ca47675a71d48b47d821e7d01223
    Author: Douwe <douwewesterdijk@gmail.com>
    Date:   Thu Mar 20 16:34:15 2025 +0100
    
        [+] SceneStation opens a scene
    
    commit 727ef97cd065463e199b9b6b520d1b999e4cbd5d
    Author: Douwe <douwewesterdijk@gmail.com>
    Date:   Thu Mar 20 16:01:22 2025 +0100
    
        [+] Warning from Station prints gameobject name
    
    commit 62ab4e13894ab5fb93880eada2b1d35a20bfa127
    Author: Douwe <douwewesterdijk@gmail.com>
    Date:   Thu Mar 20 15:56:59 2025 +0100
    
        [+] Interactable/Scene/Base Station classes
    
    commit 25bf4bc62d771fb2c5bcff7acdfa138d6a26d1c9
    Author: Douwe <douwewesterdijk@gmail.com>
    Date:   Wed Mar 19 14:17:41 2025 +0100
    
        intermediary commit
    
        still working on station interaction
    
    commit a77526bf7485ec5e2fa114769561e0874f8a0d86
    Author: Douwe <douwewesterdijk@gmail.com>
    Date:   Wed Mar 19 12:48:31 2025 +0100
    
        Begin of station interaction
    
    commit baf71fb84cd79fe772ee57822d36b1c1435def36
    Author: Douwe <douwewesterdijk@gmail.com>
    Date:   Fri Mar 14 13:24:52 2025 +0100
    
        [+] Player movement + sprites
    
    commit 7998779ad9a6ab653544c21b93e35bed8f0d568e
    Merge: 7cee6b6 f56e8af
    Author: Douwe <douwewesterdijk@gmail.com>
    Date:   Fri Mar 14 12:41:01 2025 +0100
    
        Merge branch 'Map' of https://github.com/Medikenji/Meesterproef into Map
    
    commit 7cee6b6d32ced92e10e06408e61cc32a63c2e67f
    Author: Douwe <douwewesterdijk@gmail.com>
    Date:   Fri Mar 14 12:40:58 2025 +0100
    
        [+] Basic map + begin player anim
    
        Player anim moet nog af, moet ff goed zitten en vids kijken over hoe je dit moet maken
    
    commit f56e8afa0a289010c172b0c3a2a1802487f01336
    Author: Nolan Bijmholt <120372510+Medikenji@users.noreply.github.com>
    Date:   Fri Mar 14 12:12:22 2025 +0100
    
        [+] Added working camera
    
        [+] Also added correct menu in input manager
    
    commit f18d42ea2535508402aecb8113b349ad21ad6a8b
    Author: Douwe <douwewesterdijk@gmail.com>
    Date:   Fri Mar 14 11:25:02 2025 +0100
    
        [+] Tilemap
    
        [+] We hebben een tilemap die werkt, meer assets incoming

--- simmac/Assets/Scenes/GameScene/Scripts/Player.cs
+++ simmac/Assets/Scenes/GameScene/Scripts/Player.cs
@@ -4,32 +4,23 @@ using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    [SerializeField] private float _setSpeed = 8;
     private Rigidbody2D _rb;
+    private Animator _animator;
     private Vector2 _velocity;
-    private Collider2D _torsoCol;
-    private GameObject _torso;
-    private Collider2D _legsCol;
-    private GameObject _legs;
-    [SerializeField] private float _setSpeed;
-    private float _speed;
     private InputAction _move;
+    private float _speed;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _torso = GameObject.Find("Torso");
-        _legs = GameObject.Find("Legs");
-        _torsoCol = _torso.GetComponent<CapsuleCollider2D>();
-        _legsCol = _legs.GetComponent<CapsuleCollider2D>();
+        _animator = GetComponent<Animator>();
         _move = InputSystem.actions.FindAction("Move");
     }
 
     void Update()
     {
-        CheckForSlow();
         HandleInput();
-        HandleLegRotation();
-        HandleTorsoRotation();
         _speed = _setSpeed;
         _rb.linearVelocity = _velocity;
     }
@@ -37,42 +28,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void HandleInput()
     {
         Vector2 MoveValue = _move.ReadValue<Vector2>();
+
+        if (MoveValue != Vector2.zero)
+        {
+            _animator.SetFloat("LastInputX", MoveValue.x);
+            _animator.SetFloat("LastInputY", MoveValue.y);
+        }
+
         MoveValue *= _speed;
         MoveValue = Vector2.ClampMagnitude(MoveValue, _speed);
         _velocity = MoveValue;
     }
-
-    void HandleLegRotation()
-    {
-        if (_velocity != Vector2.zero)
-        {
-            float angle = Mathf.Atan2(_velocity.x, _velocity.y) * Mathf.Rad2Deg;
-            _legs.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
-        }
-    }
-
-    void HandleTorsoRotation()
-    {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = mousePosition - _torso.transform.position;
-        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
-        _torso.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
-    }
-
-    void CheckForSlow()
-    {
-        OnTriggerStay2D(_torsoCol);
-        OnTriggerStay2D(_legsCol);
-    }
-
-    void OnTriggerStay2D(Collider2D col)
-    {
-        if (col == _torsoCol || col == _legsCol)
-        {
-            return;
-        }
-        _speed *= 0.8f;
-    }
 }
-
-
